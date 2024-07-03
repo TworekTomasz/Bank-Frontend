b5cb1a3b4b85a3d7872269394253c62c1ee321f5
Tomasz Tworek <tomasztworek27@gmail.com>
2024-07-03 15:21:38 +0200

    Added arrow and font-awesome

--- src/account/TransactionExample.js
+++ src/account/TransactionExample.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import AccountSelector from "./AccountSelector";
 import "./TransactionExample.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const TransactionExample = ({ accounts }) => {
 
-    const [selectedAccount, setSelectedAccount] = useState(null);
+    const [selectedAccountFrom, setSelectedAccountFrom] = useState(null);
+    const [selectedAccountTo, setSelectedAccountTo] = useState(null);
 
-    const handleAccountSelect = (accountId) => {
+    const handleAccountSelectFrom = (accountId) => {
         const selectedAccount = accounts.find(account => account.id === accountId);
-        setSelectedAccount(selectedAccount);
+        setSelectedAccountFrom(selectedAccount);
     };
 
+    const handleAccountSelectTo = (accountId) => {
+        const selectedAccount = accounts.find(account => account.id === accountId);
+        setSelectedAccountFrom(selectedAccount);
+    };
     return (
         <>
             <h1>Transaction Example</h1>
             <div className="transaction-selectors">
-                <AccountSelector accounts={accounts} onAccountSelect={handleAccountSelect} />
-                <AccountSelector accounts={accounts} onAccountSelect={handleAccountSelect} />
+                <AccountSelector accounts={accounts} onAccountSelect={handleAccountSelectFrom} />
+                <FontAwesomeIcon icon={faArrowRight} />
+                <AccountSelector accounts={accounts} onAccountSelect={handleAccountSelectTo} />
             </div>
         </>
     );
