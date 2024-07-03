c76806b2f3665a58db19e105a38596e1a517e224
Tomasz Tworek <tomasztworek27@gmail.com>
2024-07-03 15:44:25 +0200

    Created transation form

--- src/account/TransactionExample.js
+++ src/account/TransactionExample.js
@@ -3,21 +3,46 @@ import AccountSelector from "./AccountSelector";
 import "./TransactionExample.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import axios from "axios";
 
 const TransactionExample = ({ accounts }) => {
 
     const [selectedAccountFrom, setSelectedAccountFrom] = useState(null);
     const [selectedAccountTo, setSelectedAccountTo] = useState(null);
+    const [transactionAmount, setTransactionAmount] = useState(0);
 
-    const handleAccountSelectFrom = (accountId) => {
-        const selectedAccount = accounts.find(account => account.id === accountId);
+    const handleAccountSelectFrom = (accountNumber) => {
+        const selectedAccount = accounts.find(account => account.accountNumber === accountNumber);
         setSelectedAccountFrom(selectedAccount);
     };
 
-    const handleAccountSelectTo = (accountId) => {
-        const selectedAccount = accounts.find(account => account.id === accountId);
-        setSelectedAccountFrom(selectedAccount);
+    const handleAccountSelectTo = (accountNumber) => {
+        const selectedAccount = accounts.find(account => account.accountNumber === accountNumber);
+        setSelectedAccountTo(selectedAccount);
     };
+
+    const makeTransation = async () => {
+        if (!selectedAccountFrom || !selectedAccountTo || transactionAmount <= 0) {
+            console.error("Invalid transaction parameters");
+            return;
+        }
+
+        //TODO: Call external, API use selectedAccountFrom and selectedAccountTo to update proper values
+        try {
+            // My idea 
+            const response = await axios.post('http://your-api-endpoint/transactions', {
+                sourceAccount: selectedAccountFrom.accountNumber,
+                destinationAccount: selectedAccountTo.accountNumber,
+                amount: transactionAmount,
+                description: `Transfer from ${selectedAccountFrom.accountNumber} to ${selectedAccountTo.accountNumber}`
+            });
+
+            console.log("Transaction was made:", response.data);
+        } catch (error) {
+            console.error("Error making transaction:", error.message);
+        }
+    }
+
     return (
         <>
             <h1>Transaction Example</h1>
@@ -26,6 +51,18 @@ const TransactionExample = ({ accounts }) => {
                 <FontAwesomeIcon icon={faArrowRight} />
                 <AccountSelector accounts={accounts} onAccountSelect={handleAccountSelectTo} />
             </div>
+            <div className="transation-amount">
+                <label id="transation-amount" htmlFor={"transation-amount-input"}>Enter Amount</label>
+                <input 
+                    id="transaction-amount-input"
+                    type="number"
+                    value={transactionAmount}
+                    onChange={(e) => setTransactionAmount(parseFloat(e.target.value))}
+                />
+                <button onClick={makeTransation}>Make transaction</button>
+            </div>
+            <div>{selectedAccountFrom && selectedAccountFrom.accountNumber}</div>
+            <div>{selectedAccountTo && selectedAccountTo.accountNumber}</div>
         </>
     );
 };
